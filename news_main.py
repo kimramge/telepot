59f5aa49943ac56f7e2c2e118c75c2031953d612
kimramge <gnlckd7416@gmail.com>
2020-12-24 18:54:12 +0900

    Initial commit

---
+++ news_main.py
@@ -0,0 +1,114 @@
+from collections import deque
+
+import telepot
+from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardButton as BT
+from telepot.namedtuple import InlineKeyboardMarkup as MU
+from news_return import *
+
+token = '1476250644:AAEtz9YtAC88FNiY5AwWUKjdZEnUAGTVdjA'
+#mc = '1269898121'
+
+help_masege = "명령어를 받습니다.\n받고싶은 속보의 키워드를 정해주세요\nex)'전기차' '삼성' '코스피' 3가지의 속보를 받고싶다면\n@전기차@삼성@주가 <= 이렇게 입력해주세요"
+bot = telepot.Bot(token)
+loop = 0
+btn_chek = 0
+
+def btn_show(msg):
+    btn1 = BT(text = "1. GO",callback_data = "1")
+    btn2 = BT(text = "2. NO",callback_data = "2")
+    mu = MU(inline_keyboard = [[btn1,btn2]])
+    global MSG
+    MSG = bot.sendMessage(msg['from']['id'], "go 누르면 시작한다(아무 메세지나 보내면 멈춤)",reply_markup=mu)
+    global btn_chek
+    btn_chek = 1
+
+
+
+def query_ans(msg):
+    query_data = msg["data"]
+    if query_data == "1":
+        bot.editMessageText(telepot.message_identifier(MSG), "시작")
+        global loop
+        loop = 1
+
+
+
+    elif query_data =="2":
+        bot.editMessageText(telepot.message_identifier(MSG),help_masege)
+    global btn_chek
+    btn_chek = 0
+
+def handle(msg):
+    global btn_chek
+    if btn_chek == 1:
+        bot.deleteMessage(telepot.message_identifier(MSG))
+        btn_chek = 0
+    print(msg)
+    global mc
+    mc = (msg['from']['id'])
+    global loop
+    loop = 0
+    str1 = msg['text']
+    str_list = []
+    ntr_list = []
+    real_list = []
+    fact = 1
+    print("입력받은 메세지 : ", str1)
+    a = str1.find("@")
+    b = str1.find("@")
+    if a != -1:
+        str_list.append(a)
+        while str1[a + 1:].find('@') != -1:
+            a = str1[a + 1:].find('@') + a + 1
+            str_list.append(a)
+            ntr_list.append(str1[b:a])
+            b = a
+        ntr_list.append(str1[a:len(str1)])
+    for i in ntr_list:
+        I = i[1:]
+        I = str(I).strip()
+        real_list.append(I)
+        if len(str(i).replace(" ", "")) == 1: fact = 0
+
+    print(real_list)
+    if len(real_list) > 0 and fact == 1:
+        bot.sendMessage(msg['from']['id'],"----키워드----")
+        for i in real_list:
+            bot.sendMessage(msg['from']['id'],'-' + i + '-')
+        global keyword
+        keyword = real_list
+        btn_show(msg)
+
+    else:
+        bot.sendMessage(msg['from']['id'],"받고싶은 속보의 키워드를 정해주세요\nex)'코로나' '삼성' '코스피' 3가지의 속보를 받고싶다면\n@코로나@삼성@주가 <= 이렇게 입력해주세요")
+
+
+MessageLoop(bot,{'chat' : handle,'callback_query' : query_ans}).run_as_thread()
+
+
+while 1:
+    link_list = deque(maxlen=40)
+    chek_list = []
+    while loop:
+        time.sleep(3)
+        test_list = []
+        remak = mak(keyword)
+        for i in remak:
+            if i not in link_list:
+                test_list.append(i)
+                chek_list.append(i[1])
+                print(i, "를 test에 추가 하였습니다.")
+            else:
+                print(i, "는 이미 보냈음")
+        for i in test_list:
+            try:
+                bot.sendMessage(mc, text=i[1])
+                link_list.append(i)
+                time.sleep(3)
+            except Exception as e:
+                print(e)
+                print(type(e))
+        print(len(chek_list) - len(set(chek_list)))
+    pass
+
---
+++ news_return.py
@@ -0,0 +1,31 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import time
+
+
+def mak(kword):
+    time.sleep(0.5)
+    news = []
+    raw = requests.get("https://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=001",
+                       headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"})
+    html = BeautifulSoup(raw.text, "html.parser")
+    articles = html.select("div > ul > li > dl > dt > a")
+
+    for i in articles:
+        if "img" in str(i):
+            pass
+        else:
+            link = i['href']
+            title = (i.text).strip()
+            #news.append([title, link])
+
+            for j in range(0, len(kword)):
+                if kword[j] in title:
+                    news.append([title,link])
+
+                    break
+
+
+    return news
